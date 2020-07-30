a37c885619a1e9fc9736c78177e1b2e9b0c82ebc
greatofdream <greatofdream@gmail.com>
2020-07-31 00:01:14 +0800

    add args;token

--- factorAnalysisExample/faDWF.py
+++ factorAnalysisExample/faDWF.py
@@ -5,6 +5,19 @@ log = 'begin log\n'
 host='http://101.6.15.212:9503'
 jwt='eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImV4cCI6MTU5NTc2MTMyN30.J9Z3zbqz-kvrBJQh-0vxkx4DvSK720As6MX6ZIIZAJB9qNhSutcWPadzSX04g6PR9M9UnyItAMtohvdgmxMD7w'
 header={"Authorization": jwt, "Content-Type": "application/json"}
+r = requests.get(host+'/dwf/v1/app/login?userName={}&password={}'.format('admin','123456'),
+    headers=header)
+# update jwt
+import json
+jwt = r.json()['data']
+print(jwt)
+parser = argparse.ArgumentParser()
+header={"Authorization": jwt, "Content-Type": "application/json"}
+
+parser.add_argument('-i', dest='oids', nargs='+',default='', help='select data oids')
+args = parser.parse_args()
+print(args)
+'''
 try:
     tempPipe = os.popen('pip3 list')
     packageList = tempPipe.read()
@@ -22,24 +35,43 @@ for package in packages:
             log += templog+'\n'
         except Exception as e:
             log += templog+'\n'
+'''
 r = requests.post(host+'/dwf/v1/omf/entities/Script/objects-update',
     headers=header,
     json=[{
     "oid": "BB9F2EBEBA85B645830793ECB220AFC1",
     "outlog": log
   }])
+print(r.json()['data'])
+#import pandas as pd, numpy as np
 
-r= requests.post(host+'/dwf/v1/omf/entities/Data/objects',
+if len(args.oids)==0:
+    print('no select oid')
+    log += 'no select oid'+'\n'
+    r= requests.post(host+'/dwf/v1/omf/entities/Data/objects',
+        headers=header,
+        json={
+            "condition" : "and obj.year>={} and obj.year<{}".format(2012, 2019)
+        })
+else:
+    log += 'select oid:{}'.format(args.oids)+'\n'
+    r= requests.post(host+'/dwf/v1/omf/entities/Data/objects',
+        headers=header,
+        json={
+            "condition" : "and obj.oid in ({})".format(','.join(args.oids))
+        })
+r = requests.post(host+'/dwf/v1/omf/entities/Script/objects-update',
     headers=header,
-    json={
-        "condition" : "and obj.year>={} and obj.year<{}".format(2012, 2019)
-    })
-import pandas as pd, numpy as np
-import json
-#print(r.json()['data'])
+    json=[{
+    "oid": "BB9F2EBEBA85B645830793ECB220AFC1",
+    "outlog": log
+  }])
+print(r.json())
 datajson = r.json()['data']
 sData = pd.read_json(json.dumps(datajson, ensure_ascii=False), orient='records')
+# print(r.json()['data'])
 print(sData)
+
 r = requests.post(host+'/dwf/v1/omf/entities/Script/objects-update',
     headers=header,
     json=[{
