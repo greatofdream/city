6ace4ad60b8d50e19afc655af0de30e56e1b2c9e
zaq15 <875136184@qq.com>
2020-08-03 08:36:11 +0800

    test the web browser loading feature

--- spider/spider.js
+++ spider/spider.js
@@ -1,11 +1,18 @@
 //npm install -g cnpm --registry=https://registry.npm.taobao.org
 const puppeteer = require('puppeteer');
- 
+const process = require('process');
+// node spider.js  http://tjj.sh.gov.cn/tjnj/nj19.htm?d1=2019tjnj/C0201.htm
+var url = process.argv[2]
+console.log(url)
+if (url===null){
+  console.log('please use: node spider.js url')
+  exit(0)
+}
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://zhihu.com');
+  await page.goto(url);
   await page.screenshot({path: './example.png'});
-  await page.pdf({path:'./example.pdf', format:'A4'})
+  // await page.pdf({path:'./example.pdf', format:'A4'})
   await browser.close();
 })();
